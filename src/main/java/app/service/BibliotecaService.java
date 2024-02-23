15fe3d8b6fe9caca6bfc46063ecad9fc9fe69ae8
Octavio <tavinhoblanco@uol.com.br>
2024-02-23 19:59:17 -0300

    Fix Service

--- src/main/java/app/service/BibliotecaService.java
+++ src/main/java/app/service/BibliotecaService.java
@@ -4,10 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import app.entity.Biblioteca;
 import app.repository.BibliotecaRepository;
-
+@Service
 public class BibliotecaService {
 
 	@Autowired
